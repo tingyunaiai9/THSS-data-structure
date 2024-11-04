8cd263d12cc50bc91d8d62a3bc248ba689980140
tingyunaiai9 <2920825585@qq.com>
2024-11-04 16:48:54 +0800

    add binary tree unfinished of assignment2
    Signed-off-by: tingyunaiai9 <2920825585@qq.com>

---
+++ oj/Assignment-2/binary_tree.cpp
@@ -0,0 +1,65 @@
+#include <cstdio>
+
+// #define LEN 4000000
+#define LEN 10
+
+int pre[LEN], in[LEN], post[LEN];
+
+int search(int *array, int x, int left, int right); // 在无序array的[left, right)区间内查找x，返回下标
+
+void build_tree(int in_left, int in_right,
+                int pre_left, int pre_right,
+                int post_left, int post_right); // 左闭右开
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &pre[i]);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &post[i]);
+
+    build_tree(0, n, 0, n, 0, n);
+    for (int i = 0; i < n; i++)
+        printf("%d ", in[i]);
+
+    return 0;
+}
+
+int search(int *array, int x, int left, int right)
+{
+    for (int i = left; i < right; i++)
+    {
+        if (array[i] == x)
+            return i;
+    }
+
+    return -1;
+}
+
+void build_tree(int in_left, int in_right,
+                int pre_left, int pre_right,
+                int post_left, int post_right)
+{
+    if (in_left >= in_right)
+        return;
+
+    int root = pre[pre_left];
+    int left = pre[pre_left + 1];
+    int right = post[post_right - 2];
+
+    int left_index = search(post, left, post_left, post_right);
+    // 左子树长度 left_index - post_left + 1
+    build_tree(in_left, in_left + left_index - post_left + 1,
+               pre_left + 1, pre_left + left_index - post_left + 2,
+               post_left, left_index + 1);
+
+    in[in_left + left_index - post_left + 1] = root;
+
+    int right_index = pre_left + left_index - post_left + 2;
+    // 右子树长度 pre_right - right_index
+    build_tree(in_left + left_index - post_left + 2, in_right,
+               right_index, pre_right,
+               left_index + 1, post_right - 1);
+}
